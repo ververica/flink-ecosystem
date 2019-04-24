bc6334b2dd98455e7cb94f2afa0a01b0caf1f104
Robert Metzger <rmetzger@apache.org>
2019-04-24 14:26:36 +0200

    Turn repo into a multi-module maven project

---
+++ flink-ecosystem-backend/src/main/java/org/apache/flink/community/FCPackageController.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.community;
+
+import org.apache.flink.community.model.FCPackage;
+import org.apache.flink.community.service.FCPackageService;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1/packages")
+public class FCPackageController {
+	@Autowired
+	private FCPackageService packageService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+	public String getTeams() throws JsonProcessingException {
+		System.out.println("Come here");
+		List<FCPackage> packages = packageService.findAllPackages();
+		return new ObjectMapper().writeValueAsString(packages);
+	}
+
+	@RequestMapping(value = "{inputPackage}", method = RequestMethod.GET, produces = "application/json")
+	public String getPackage(@PathVariable("inputPackage") String pack) throws JsonProcessingException {
+		System.out.println("Get package." + pack);
+
+		return "Have received" + pack;
+	}
+
+	@RequestMapping(value = "{updatePackage}", method = RequestMethod.PATCH, produces = "application/json")
+	public String updatePackage(@PathVariable("updatePackage") String packName,
+								@RequestBody FCPackage pack) {
+		FCPackage old = packageService.findPackageByName(packName);
+		packageService.savePackage(old.update(pack));
+		return old.toString();
+	}
+
+	@RequestMapping(value = "{deletePackage}", method = RequestMethod.DELETE, produces = "application/json")
+	public String deletePackage(@PathVariable("deletePackage") String packName) {
+		FCPackage old = packageService.findPackageByName(packName);
+		old.setValid(false);
+		packageService.savePackage(old);
+		return "delete";
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/json")
+	public String addPackage(@RequestBody FCPackage pack) {
+		System.out.println("add package" + pack);
+		packageService.savePackage(pack);
+		return "add package " + pack;
+	}
+}
+
---
+++ flink-ecosystem-backend/src/main/java/org/apache/flink/community/model/FCPackage.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.community.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "fcpackage")
+public class FCPackage {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	private String name;
+
+	private String description;
+
+	private String websiteUrl;
+
+	private String repoUrl;
+
+	private String license;
+
+	private Boolean valid = true;
+
+	public FCPackage update(FCPackage other) {
+		if (other.name != null) {
+			this.name = other.name;
+		}
+
+		if (other.description != null) {
+			this.description = other.description;
+		}
+
+		if (other.websiteUrl != null) {
+			this.websiteUrl = other.websiteUrl;
+		}
+
+		if (other.repoUrl != null) {
+			this.repoUrl = other.repoUrl;
+		}
+
+		if (other.license != null) {
+			this.license = other.license;
+		}
+		return this;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setValid(Boolean valid) {
+		this.valid = valid;
+	}
+
+	@Override
+	public String toString() {
+		return "Package: {" +
+				"id: " + id +
+				"name: " + name +
+				"description: " + description +
+				"websiteUrl: " + websiteUrl +
+				"repoUrl: " + repoUrl +
+				"license: " + license +
+				"}";
+	}
+}
+
