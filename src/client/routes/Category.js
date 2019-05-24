32845f35ccc86af415dba8561baef87632f2ce68
Daryl Roberts <daryl.roberts@gmail.com>
2019-05-24 17:17:50 +0200

    [boilerplate] make space for category

--- src/client/routes/Category.js
+++ src/client/routes/Category.js
@@ -1,5 +1,14 @@
 import React from "react";
+import MainCard from "client/components/MainCard";
 
 export default function Category(props) {
-  return <div>category: {props.category} </div>;
+  return (
+    <MainCard
+      header={
+        <h2 className="h5 mb-0">Packages tagged with "{props.category}"</h2>
+      }
+    >
+      {null}
+    </MainCard>
+  );
 }
