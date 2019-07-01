2d0630e21bd0eacd971fbd125eb926b1095c1568
Daryl Roberts <daryl.roberts@gmail.com>
2019-07-01 18:40:43 +0200

    initial search code

--- src/client/routes/Search.tsx
+++ src/client/routes/Search.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import MainCard from "client/components/MainCard";
 import { RouteComponentProps } from "@reach/router";
+import useFetchData from "client/helpers/useFetchData";
 
 // @TODO search name, description, readme, tags
 export default function Search(props: Props) {
+  const packages = useFetchData(`/api/v1/search/${props.searchQuery}`);
+
   return (
     <MainCard header={`Search results for "${props.searchQuery}"`}>
-      nothing is here
+      {JSON.stringify(packages)}
     </MainCard>
   );
 }
