e088f02356cdf2d4601f3264a61a3d5350bf1cee
Daryl Roberts <daryl.roberts@gmail.com>
2019-07-02 17:47:23 +0200

    upload image requires user

--- src/server/routes/api/v1/upload-image.js
+++ src/server/routes/api/v1/upload-image.js
@@ -1,27 +1,31 @@
 import fs from "fs";
+import checkUser from "server/middleware/checkUser";
 
-exports.post = async ctx => {
-  const { image } = ctx.request.files;
-  const file = fs.readFileSync(image.path);
-  const previousId = ctx.request.get("x-previous-image");
+exports.post = [
+  checkUser(),
+  async ctx => {
+    const { image } = ctx.request.files;
+    const file = fs.readFileSync(image.path);
+    const previousId = ctx.request.get("x-previous-image");
 
-  try {
-    const [image_id] = await ctx.db("image").insert({
-      file_type: image.type,
-      file_size: image.size,
-      file,
-    });
+    try {
+      const [image_id] = await ctx.db("image").insert({
+        file_type: image.type,
+        file_size: image.size,
+        file,
+      });
 
-    const removed = await ctx
-      .db("image")
-      .where({ id: previousId })
-      .delete();
+      const removed = await ctx
+        .db("image")
+        .where({ id: previousId })
+        .delete();
 
-    ctx.body = { image_id, removed };
-  } catch (e) {
-    ctx.throw(400, {
-      id: "image",
-      message: "there was a problem uploading your image.",
-    });
-  }
-};
+      ctx.body = { image_id, removed };
+    } catch (e) {
+      ctx.throw(400, {
+        id: "image",
+        message: "there was a problem uploading your image.",
+      });
+    }
+  },
+];
