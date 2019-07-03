26ba238d22586a2b5269feae32f579a76a1677c6
Daryl Roberts <daryl.roberts@gmail.com>
2019-07-03 13:12:22 +0200

    add contact us link in Footer
    fixes #12

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 
 export default function Footer() {
-  // @TODO add "contact us" #12
   return (
     <div className="row no-gutters text-center d-block py-4">
       <p>
-        Copyright © 2014-2019{" "}
-        <a href="https://apache.org/">The Apache Software Foundation</a>. All
-        Rights Reserved.
+        Copyright ©2014-2019&nbsp;
+        <a href="https://apache.org/" rel="noreferrer noopener nofollow">
+          The Apache Software Foundation
+        </a>
+        . All Rights Reserved.
       </p>
       <p>
         Apache Flink, Flink®, Apache®, and the squirrel logo are either
         registered trademarks or trademarks of The Apache Software Foundation.
       </p>
       <p>
-        <a href="https://flink.apache.org/privacy-policy.html">
+        <a
+          href="https://flink.apache.org/privacy-policy.html"
+          rel="noreferrer noopener nofollow"
+        >
           Privacy Policy
+        </a>
+        &nbsp;|&nbsp;
+        <a
+          href="https://flink.apache.org/community.html#mailing-lists"
+          rel="noreferrer noopener nofollow"
+        >
+          Contact Us
         </a>
       </p>
     </div>
