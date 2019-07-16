23ce04cc0aeee2d1c68a7609d6ba762bd64ee744
Daryl Roberts <daryl.roberts@gmail.com>
2019-07-16 11:08:44 +0200

    take the mysql database from the ENV

--- src/server/main.js
+++ src/server/main.js
@@ -40,7 +40,7 @@ const db = knex({
     host: process.env.MYSQL_HOST || "127.0.0.1",
     user: process.env.MYSQL_USER || "root",
     password: process.env.MYSQL_PASSWORD || "test",
-    database: "flink_ecosystem",
+    database: process.env.MYSQL_DATABASE || "flink_ecosystem",
   },
 });
 
