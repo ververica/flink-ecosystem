2c692f827bbfa78406acb8be938ba096dfc7307a
Daryl Roberts <daryl.roberts@gmail.com>
2019-07-18 15:38:45 +0200

    update docker set up for running locally

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = app => {
-  app.use(proxy("/api", { target: "http://localhost:4000" }));
-  app.use(proxy("/auth", { target: "http://localhost:4000" }));
+  app.use(proxy("/api", { target: "http://server:4000" }));
+  app.use(proxy("/auth", { target: "http://server:4000" }));
 };
