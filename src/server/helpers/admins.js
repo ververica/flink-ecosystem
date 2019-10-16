a105636aecdc4a69878053b2fa3cab291022ffa7
Robert Metzger <89049+rmetzger@users.noreply.github.com>
2019-10-16 13:47:33 +0200

    Remove non VV admins from list
    I changed this because we will host this in our name now.

--- src/server/helpers/admins.js
+++ src/server/helpers/admins.js
@@ -4,23 +4,11 @@
 export const admins = [
   1727146, // StephanEwen
   5756858, // tillrohrmann
-  105434, // hsaputra
-  5880972, // gyfora
   1756620, // uce
   2388347, // fhueske
   5725237, // zentol
   68551, // aljoscha
   89049, // rmetzger
-  5746567, // twalthr
-  5990983, // mbalassi
-  5284370, // tzulitai
-  569655, // greghogan
-  1826769, // warneke
-  498957, // vasia
-  332352, // alanfgates
-  837221, // mxm
-  9400874, // shaoxuan-wang
-  22488084, // sunjincheng121
 ];
 
 if (process.env.NODE_ENV !== "production") {
