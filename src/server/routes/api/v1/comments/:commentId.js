cc46da6e9cfbb68ce7c584ae9305a1daff011b0c
Robert Metzger <rmetzger@apache.org>
2019-10-28 10:41:28 +0100

    Fix formatting

--- src/server/routes/api/v1/comments/:commentId.js
+++ src/server/routes/api/v1/comments/:commentId.js
@@ -5,78 +5,78 @@ import { parseValidatorError } from "server/helpers/parseValidatorError";
 import { commentSchema } from "server/helpers/validatorSchemas";
 import { checkAccess } from "server/helpers/admins";
 
-const checkCommentOwner = () => async(ctx, next) => {
-    const { commentOwnerId } = await ctx
-        .db("comment")
-        .select("user_id as commentOwnerId")
-        .where({ id: ctx.params.commentId })
-        .first();
+const checkCommentOwner = () => async (ctx, next) => {
+  const { commentOwnerId } = await ctx
+    .db("comment")
+    .select("user_id as commentOwnerId")
+    .where({ id: ctx.params.commentId })
+    .first();
 
-    checkAccess(ctx, commentOwnerId);
+  checkAccess(ctx, commentOwnerId);
 
-    return next();
+  return next();
 };
 
 exports.delete = [
-    checkUser(),
-    checkCommentOwner(),
-    async ctx => {
-        const { commentId } = ctx.params;
+  checkUser(),
+  checkCommentOwner(),
+  async ctx => {
+    const { commentId } = ctx.params;
 
-        await ctx
-            .db("comment")
-            .update({ deleted: 1 })
-            .where({ id: commentId })
-            .limit(1);
+    await ctx
+      .db("comment")
+      .update({ deleted: 1 })
+      .where({ id: commentId })
+      .limit(1);
 
-        ctx
-            .sendMail(
-                "comment-deleted",
-                `Deleted comment id = "${commentId}"`,
-                "end of message"
-            )
-            .catch(e => {
-                console.log(e);
-            });
-        ctx.body = { commentId };
-    },
+    ctx
+      .sendMail(
+        "comment-deleted",
+        `Deleted comment id = "${commentId}"`,
+        "end of message"
+      )
+      .catch(e => {
+        console.log(e);
+      });
+    ctx.body = { commentId };
+  },
 ];
 
 exports.post = [
-    checkUser(),
-    checkCommentOwner(),
-    async ctx => {
-        const { commentId } = ctx.params;
-        const { text, packageSlug, packageName } = ctx.request.body;
+  checkUser(),
+  checkCommentOwner(),
+  async ctx => {
+    const { commentId } = ctx.params;
+    const { text, packageSlug, packageName } = ctx.request.body;
 
-        const validation = Joi.validate({ text, packageSlug }, commentSchema);
-        if (validation.error) {
-            ctx.throw(400, parseValidatorError(validation.error.message));
-        }
+    const validation = Joi.validate({ text, packageSlug }, commentSchema);
+    if (validation.error) {
+      ctx.throw(400, parseValidatorError(validation.error.message));
+    }
 
-        await ctx
-            .db("comment")
-            .update({ text, updated: ctx.db.raw("now()") })
-            .where({ id: commentId })
-            .limit(1);
+    await ctx
+      .db("comment")
+      .update({ text, updated: ctx.db.raw("now()") })
+      .where({ id: commentId })
+      .limit(1);
 
-        ctx
-            .sendMail(
-                "comment-edited",
-                `Edited comment on package "${packageName}"`,
-                commentMailerTemplate({
-                    packageName,
-                    userName: ctx.state.user.login,
-                    text,
-                    origin: ctx.request.origin,
-                    packageSlug,
-                    commentId,
-                })
-            )
-            .catch(e => {
-                console.log(e);
-            });
+    ctx
+      .sendMail(
+        "comment-edited",
+        `Edited comment on package "${packageName}"`,
+        commentMailerTemplate({
+          packageName,
+          userName: ctx.state.user.login,
+          text,
+          origin: ctx.request.origin,
+          packageSlug,
+          commentId,
+        })
+      )
+      .catch(e => {
+        console.log(e);
+      });
 
-        ctx.body = { commentId, text };
-    },
+    ctx.body = { commentId, text };
+  },
 ];
